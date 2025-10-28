815656d5c155b581226102ec40d1dfd3fa5a7293
LI MOU <lxglbk@gmail.com>
2025-10-28 04:27:34 +0000

    support allgather

--- cumask/benchmark_rccl_hipblaslt_cumask.py
+++ cumask/benchmark_rccl_hipblaslt_cumask.py
@@ -15,17 +15,21 @@ from primus_turbo.pytorch.core import TurboStream
 def benchmark(
     matmul_size,
     comm_size,
+    comm_op,
     matmul_stream,
     comm_stream,
     world_size,
     warmup_steps=10,
     benchmark_steps=30,
 ):
+    num_comm_elements = (comm_size[0] + 3) // 4
     torch.cuda.empty_cache()
     with torch.device("cuda"):
         A = torch.randn(*matmul_size[0], dtype=torch.bfloat16)
         B = torch.randn(*matmul_size[1], dtype=torch.bfloat16)
-        comm_tensor = torch.randn(*comm_size, dtype=torch.float32)
+        comm_tensor = torch.randn((num_comm_elements,), dtype=torch.float32)
+        if args.comm_op == "all_gather":
+            output_tensor = torch.empty((num_comm_elements * world_size,), dtype=torch.float32)
 
     # warmup matmul
     for _ in range(warmup_steps):
@@ -45,10 +49,16 @@ def benchmark(
 
     matmul_time = start_event.elapsed_time(end_event) / benchmark_steps
 
-    # warmup comm (all_reduce)
+    def do_comm_operation():
+        if args.comm_op == "all_gather":
+            dist.all_gather_into_tensor(output_tensor, comm_tensor)
+        else:
+            dist.all_reduce(comm_tensor)
+
+    # warmup comm
     for _ in range(warmup_steps):
         with torch.cuda.stream(comm_stream):
-            dist.all_reduce(comm_tensor)
+            do_comm_operation()
     torch.cuda.synchronize()
 
     # benchmark comm
@@ -57,7 +67,7 @@ def benchmark(
     start_event.record(comm_stream)
     for _ in range(benchmark_steps):
         with torch.cuda.stream(comm_stream):
-            dist.all_reduce(comm_tensor)
+            do_comm_operation()
     end_event.record(comm_stream)
     torch.cuda.synchronize()
 
@@ -68,7 +78,7 @@ def benchmark(
         with torch.cuda.stream(matmul_stream):
             torch.matmul(A, B)
         with torch.cuda.stream(comm_stream):
-            dist.all_reduce(comm_tensor)
+            do_comm_operation()
     torch.cuda.synchronize()
 
     # benchmark matmul-comm
@@ -89,7 +99,7 @@ def benchmark(
 
         start_comm_events[i].record(comm_stream)
         with torch.cuda.stream(comm_stream):
-            dist.all_reduce(comm_tensor)
+            do_comm_operation()
         end_comm_events[i].record(comm_stream)
         torch.cuda.synchronize()
 
@@ -114,7 +124,14 @@ def parse_args():
     parser.add_argument("-n", type=int, default=4608, help="Number of columns in matrix B (GEMM)")
     parser.add_argument("-k", type=int, default=36864, help="Common dimension between matrices A and B (GEMM)")
     parser.add_argument(
-        "--comm-size", type=int, default=None, help="Number of float32 elements for communication tensor", required=True)
+        "--comm-size", type=int, default=None, help="size in bytes of communication tensor on source device" , required=True)
+    parser.add_argument(
+        '--comm-op',
+        type = str,
+        choices = ["all_reduce", "all_gather"],
+        default = "all_reduce"
+    )
+
     parser.add_argument("--num-comm-cu", type=int, default=None, help="number of CU for communication stream, default None or -1 means use all compute units")
     parser.add_argument("-p", "--profile", action="store_true", help="Enable PyTorch profiler to generate chrome trace")
     return parser.parse_args()
@@ -163,7 +180,7 @@ if __name__ == "__main__":
     comm_sizes = [(args.comm_size,)]
 
     if rank == 0:
-        print(f"Using RCCL all_reduce with world_size={world_size}")
+        print(f"Using RCCL {args.comm_op} with world_size={world_size}")
         print(f"bfloat16, Matmul: A @ B where A={matmul_sizes[0][0]}, B={matmul_sizes[0][1]}")
         print(f"float32, Comm size: {comm_sizes[0]}")
 
@@ -178,6 +195,7 @@ if __name__ == "__main__":
         matmul_time, comm_time, matmul_comm_time, overlapped_matmul_time, overlapped_comm_time = benchmark(
             matmul_size,
             comm_size,
+            args.comm_op,
             matmul_stream.torch_stream if hasattr(matmul_stream, "torch_stream") else matmul_stream,
             comm_stream.torch_stream if hasattr(comm_stream, "torch_stream") else comm_stream,
             world_size,
@@ -185,24 +203,28 @@ if __name__ == "__main__":
 
         size_A, size_B = matmul_size
         if rank == 0:
+            def get_busbw(time_elapsed):
+                algobw = args.comm_size / time_elapsed * 1000.0 * (1 if args.comm_op=="all_reduce" else world_size)
+                scale = (world_size-1) / world_size * (2 if args.comm_op=="all_reduce" else 1)
+                return algobw * scale / 1e9
             print("-" * 60)
             print(
-                f"A: {size_A[0]}x{size_A[1]} @ B: {size_B[0]}x{size_B[1]}, comm: {comm_size[0]}",
+                f"A: {size_A[0]}x{size_A[1]} @ B: {size_B[0]}x{size_B[1]}, comm: {comm_size[0]} bytes",
             )
             print(f"  matmul alone:         {matmul_time:.4f} ms")
             print("      tflops:  {:.4f}".format(2.0*args.m*args.n*args.k/matmul_time*1000/(2**40)))
             print(f"  comm alone:           {comm_time:.4f} ms")
-            print("      busbw:  {:.4f} GB/s".format(args.m*args.n*4.0/comm_time*1000*2*(world_size-1)/world_size/1e9))
+            print("      busbw:  {:.4f} GB/s".format(get_busbw(comm_time)))
             print(f"  matmul + comm:        {matmul_comm_time:.4f} ms")
             print(f"  overlapped matmul:    {overlapped_matmul_time:.4f} ms")
             print("      tflops: {:.4f}".format(2.0*args.m*args.n*args.k/overlapped_matmul_time*1000/(2**40)))
             print(f"  overlapped comm:      {overlapped_comm_time:.4f} ms")
-            print("      busbw:  {:.4f} GB/s".format(args.m*args.n*4.0/overlapped_comm_time*1000*2*(world_size-1)/world_size/1e9))
+            print("      busbw:  {:.4f} GB/s".format(get_busbw(overlapped_comm_time)))
             print("-" * 60)
 
     if rank == 0 and args.profile:
-        prof.export_chrome_trace(f"rccl_allreduce_trace_rank{rank}.json")
-        print(f"Profiler trace saved to rccl_allreduce_trace_rank{rank}.json")
+        prof.export_chrome_trace(f"rccl_{args.comm_op}_trace_rank{rank}.json")
+        print(f"Profiler trace saved to rccl_{args.comm_op}_trace_rank{rank}.json")
 
     dist.destroy_process_group()
 
